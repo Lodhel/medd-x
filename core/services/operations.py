2858bfd525d9966b4be45232c5faad0864f85d3f
Lodhel <lodhelnew@gmail.com>
2021-01-27 19:54:41 +0300

    base user models

---
+++ core/services/operations.py
@@ -0,0 +1,12 @@
+import hashlib
+import uuid
+
+
+class General:
+
+    def generate_token(self):
+        result = str(uuid.uuid4())
+        return result[0:32]
+
+    def crypt(self, main_string):
+        return hashlib.sha256(main_string.encode()).hexdigest()
--- source/models.py
+++ source/models.py
@@ -1,13 +1,14 @@
 import datetime
 
 from django.db import models
+from ..core.services.operations import General
 
 
 class Profile(models.Model):
     email = models.EmailField(max_length=32, unique=True, null=True)
-    token = models.CharField(max_length=255)
+    token = models.CharField(max_length=255, default=General().generate_token())
     is_active = models.BooleanField(default=False)
-    date_joined = models.DateTimeField(default=datetime.date.today().strftime('%Y-%m-%d %H:%M'))  # TODO check
+    date_joined = models.DateTimeField(default=datetime.date.today().strftime('%Y-%m-%d %H:%M'))
 
     def __str__(self):
         return str(self.pk)
@@ -16,4 +17,24 @@ class Profile(models.Model):
 class Patient(models.Model):
     profile = models.OneToOneField(Profile)
     secure = models.BooleanField(default=False)
-    phone = models.CharField(max_length=16)
+    phone = models.CharField(max_length=16, null=True)  # secure
+    sms_code = models.CharField(max_length=4, null=True)  # secure
+    cover_name = models.CharField(max_length=32, null=True)  # anonym
+    first_name = models.CharField(max_length=32, null=True)  # secure
+    last_name = models.CharField(max_length=32, null=True)  # secure
+    LANG_CHOICES = (
+        ('1', 'en'),
+        ('2', 'de'),
+        ('3', 'fr'),
+        ('4', 'sp'),
+        ('5', 'ru'),
+        ('6', 'pt'),
+        ('7', 'it'),
+        ('8', 'ja'),
+        ('9', 'ko'),
+        ('10', 'zh'),
+        ('11', 'tr'),
+    )
+    language = models.CharField(max_length=2, choices=LANG_CHOICES)
+    country = models.CharField(max_length=64)
+    city = models.CharField(max_length=64)
