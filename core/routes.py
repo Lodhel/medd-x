81f8099d5860a2bdfe8e3d77a71df4e99bf0ed9b
Lodhel <lodhelnew@gmail.com>
2021-02-02 20:27:44 +0300

    auth for all roles

--- core/routes.py
+++ core/routes.py
@@ -40,7 +40,7 @@ class BaseProfileLogic:
         }
 
 
-@routes.view("/auth/company/")
+@routes.view("/api/auth/company/")
 class CompanyViewSet(web.View, CorsViewMixin):
 
     def make_response(self, data, company):
@@ -66,10 +66,11 @@ class CompanyViewSet(web.View, CorsViewMixin):
             password = General().crypt(data["password"])
             profile = await Profile.query.where(Profile.email == email).gino.first()
             company = await Company.query.where(Company.profile_id == profile.id).gino.first()
-        except ZeroDivisionError:
+        except:
             return json_response(
                 {
-                    "error": "not found"
+                    "error": "not found",
+                    "success": False
                 }
             )
 
@@ -79,7 +80,146 @@ class CompanyViewSet(web.View, CorsViewMixin):
         else:
             return json_response(
                 {
-                    "error": "auth is wrong"
+                    "error": "auth is wrong",
+                    "success": False
                 }
             )
 
+
+@routes.view("/api/auth/anonym/")
+class SecureViewSet(web.View, CorsViewMixin):
+
+    def make_response(self, data, anonym):
+        return {
+            "data": {
+                **data,
+                "first_name": anonym.cover_name,
+                "step": anonym.step
+            },
+            "success": True
+        }
+
+    async def post(self):
+        data = await self.request.json()
+        await BaseView().save_cookie(self.request.headers["User-Agent"],
+                                     ["{}: {},\r\n".format(key, value) for key, value in data.items() if key != "password"],
+                                     self.request.url.raw_path_qs)
+        try:
+            email = data["email"]
+            password = General().crypt(data["password"])
+            profile = await Profile.query.where(Profile.email == email).gino.first()
+            secure = await Anonym.query.where(Anonym.profile_id == profile.id).gino.first()
+        except:
+            return json_response(
+                {
+                    "error": "not found",
+                    "success": False
+                }
+            )
+
+        if profile.password == password and profile.is_active:
+            data = BaseProfileLogic().get_data(profile)
+            return json_response(self.make_response(data, secure))
+        else:
+            return json_response(
+                {
+                    "error": "auth is wrong",
+                    "success": False
+                }
+            )
+
+
+@routes.view("/api/auth/secure/")
+class SecureViewSet(web.View, CorsViewMixin):
+
+    def make_response(self, data, secure):
+        return {
+            "data": {
+                **data,
+                "first_name": secure.first_name,
+                "last_name": secure.last_name,
+                "questionary": secure.questionary,
+                "step": secure.step
+            },
+            "success": True
+        }
+
+    async def post(self):
+        data = await self.request.json()
+        await BaseView().save_cookie(self.request.headers["User-Agent"],
+                                     ["{}: {},\r\n".format(key, value) for key, value in data.items() if key != "password"],
+                                     self.request.url.raw_path_qs)
+        try:
+            auth = data["auth"]
+            if validate_email(auth):
+                profile = await Profile.query.where(Profile.email == auth).gino.first()
+            else:
+                profile = await Profile.query.where(Profile.phone == auth).gino.first()
+            password = General().crypt(data["password"])
+            secure = await Secure.query.where(Secure.profile_id == profile.id).gino.first()
+        except:
+            return json_response(
+                {
+                    "error": "not found",
+                    "success": False
+                }
+            )
+
+        if profile.password == password and profile.is_active:
+            data = BaseProfileLogic().get_data(profile)
+            return json_response(self.make_response(data, secure))
+        else:
+            return json_response(
+                {
+                    "error": "auth is wrong",
+                    "success": False
+                }
+            )
+
+
+@routes.view("/api/auth/manager/")
+class SecureViewSet(web.View, CorsViewMixin):
+
+    def make_response(self, data, manager):
+        return {
+            "data": {
+                **data,
+                "first_name": manager.first_name,
+                "middle_name": manager.middle_name,
+                "last_name": manager.last_name,
+                "step": manager.step
+            },
+            "success": True
+        }
+
+    async def post(self):
+        data = await self.request.json()
+        await BaseView().save_cookie(self.request.headers["User-Agent"],
+                                     ["{}: {},\r\n".format(key, value) for key, value in data.items() if key != "password"],
+                                     self.request.url.raw_path_qs)
+        try:
+            auth = data["auth"]
+            if validate_email(auth):
+                profile = await Profile.query.where(Profile.email == auth).gino.first()
+            else:
+                profile = await Profile.query.where(Profile.phone == auth).gino.first()
+            password = General().crypt(data["password"])
+            secure = await Manager.query.where(Manager.profile_id == profile.id).gino.first()
+        except:
+            return json_response(
+                {
+                    "error": "not found",
+                    "success": False
+                }
+            )
+
+        if profile.password == password and profile.is_active:
+            data = BaseProfileLogic().get_data(profile)
+            return json_response(self.make_response(data, secure))
+        else:
+            return json_response(
+                {
+                    "error": "auth is wrong",
+                    "success": False
+                }
+            )
