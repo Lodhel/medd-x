2d1d733497845354f4c88d337cb84ddb44158b14
Lodhel <lodhelnew@gmail.com>
2021-02-06 21:55:13 +0300

    admin panel

--- source/admin.py
+++ source/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Company)
+admin.site.register(Manager)
+admin.site.register(Secure)
+admin.site.register(Anonym)
+admin.site.register(Cookie)
