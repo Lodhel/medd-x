9b5bae874ff74dd09e8856ddc95de7ab3ddaa854
Lodhel <lodhelnew@gmail.com>
2021-02-18 19:36:48 +0300

    questionnariemodule for mongoDB

--- source/mongo_db_services.py
+++ source/mongo_db_services.py
@@ -7,8 +7,42 @@ class BaseMongo:
 
         db = conn['knowledge_base']
 
-        coll = db["health"]
+        return db["health"]
 
-        doc = {"name": "Иван", "surname": "Иванов"}
+    def create_default_data(self):
+
+        doc = {
+            "Condition or Disease": [
+                "Excludes the presence of condition",
+                "Confirms the presence of condition",
+                "Supposes the presence of condition",
+                "Indicates low intensity of condition",
+                "Indicates middle intensity of condition",
+                "Indicates high intensity of condition",
+                "Impact not define"
+            ],
+            "Symptom": [
+                "Excludes the presence of condition",
+                "Confirms the presence of condition",
+                "Supposes the presence of condition",
+                "Indicates low intensity of condition",
+                "Indicates middle intensity of condition",
+                "Indicates high intensity of condition",
+                "Impact not define"
+            ],
+            "Human parameter": [],
+            "Lifestyle and habits": [],
+            "Diet and nutrition": [],
+            "Environment Influence": [],
+            "Anamnesis and Chronic": [],
+            "Procedure or Exam": [],
+            "Genetic heredity": [],
+            "Laboratory test": [],
+            "Treatment": [],
+            "Allergy or Asthma": [],
+            "Additional information": [],
+            "Cause of pathology": []
+        }
+        coll = self.connect()
         coll.save(doc)
 
